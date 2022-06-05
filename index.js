4c78f9e1bc858f22e033b030b8cdc2283221514e
Hyuyeong <choibeny@gmail.com>
2022-06-05 20:01:31 +1200

    make it better

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ db.once('open', () => {
   console.log('Database connected');
 });
 
+app.use(express.static(path.join(__dirname, '/todo/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/todo/build', 'index.html'));
+});
+
 app.post('/api/new', (req, res) => {
   const newTodo = new Todo(req.body);
   newTodo.save();
@@ -29,6 +35,11 @@ app.get('/api/find', async (req, res) => {
   // console.log(todo);
 });
 
-app.listen(5000, () => {
-  console.log('Serving on port 5000');
+// app.listen(5000, () => {
+//   console.log('Serving on port 5000');
+// });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('success! Serving on port 5000');
 });
